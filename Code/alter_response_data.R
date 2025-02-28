b0f0fec1366f69faf70f5a1ccd10eba9ae15e658
anniekellner <annie.kellner@colostate.edu>
2025-02-28 08:40:00 -0700

    ended up putting the working code into the Rmd doc

--- Code/alter_response_data.R
+++ Code/alter_response_data.R
@@ -1,5 +1,5 @@
 ###########################################################################
-###               PRELIMINARY MODELING                                  ###
+###               DATA PREP                                             ###
 ###########################################################################
 
 # 2-28-2025
@@ -13,19 +13,38 @@ library(tidyverse)
 
 
 
-##  ---   LOAD DATA   -----   ##
+##  ---   LOAD AND PREP DATA   -----   ##
 
 kma <- read.csv('./Data/KMA_Wx_Data_2015_2024.csv') # variables
 
-kmaFires <- read.csv("C:/Users/akellner/OneDrive - Colostate/Documents/Fire/KMA_Wx_Fire_Data/KMA_FDRS_Wildfires.csv")
+## Presence-Absence Fire Data
 
-## Manipulate Fire DF
+kmaFires <- read.csv("C:/Users/akellner/OneDrive - Colostate/Documents/Fire/KMA_Wx_Fire_Data/KMA_FDRS_Wildfires.csv")
 
 kmaFires$Date <- as.Date(kmaFires$Date, format = "%m/%d/%Y")
 
-kmaFires$Presence <- 1
+# Find duplicates (multiple fires on a single day)
 
-# Create df with all dates
+dups <- kmaFires %>%
+  group_by(Date) %>%
+  filter(n() > 1) %>%
+  arrange(Date) 
+
+dups <- dups %>%
+  add_count(Date) %>%
+  arrange(Date) %>%
+  rename(count = n)
+
+# remove duplicate rows
+
+dups <- dups %>% # reduce df to only date and count
+  select(Date, count) %>%
+  distinct()
+
+
+# Add all dates to response df
+
+# Create df with all dates represented
 
 allDates <- seq(
   from = first(kmaFires$Date),
@@ -38,11 +57,30 @@ allDates <- data.frame(Date = allDates)
 allDates <- allDates %>%
   rename(Date = allDates)
 
+# Combine with presence data
+
+kmaFires$Presence <- 1
+
+kmaFires <- kmaFires %>%
+  select(Date, Presence)
+
 kmaComplete <- kmaFires %>%
   full_join(allDates) %>%
   arrange(Date) %>% 
   replace_na(list(Presence = 0)) 
 
+kmaComplete <- kmaComplete %>%
+  select(Date, Presence)
 
-# Use 'dredge' to create global model from high-priority vars
+kmaAll <- kmaComplete %>%
+  left_join(dups)
+
+kmaAll <- kmaAll %>%
+  mutate(count = coalesce(count, Presence)) 
+
+saveRDS(kmaAll, file = "./Data/KMA/ResponseDF_02282025.Rds")
+
+
+
+
 
